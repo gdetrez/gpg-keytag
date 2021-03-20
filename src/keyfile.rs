d5fae9b5ae71dda407d6bd9fffb3942e3869ba76
Grégoire Détrez <gregoire@fripost.org>
2021-03-20 10:55:52 +0100

    Working PoC

---
+++ src/keyfile.rs
@@ -0,0 +1,155 @@
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take, take_while1},
+    character::is_digit,
+    combinator::iterator,
+    IResult,
+};
+use std::io;
+
+type Input<'a> = &'a [u8];
+type Token<'a> = &'a [u8];
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum TokenTree<'a> {
+    Node(Vec<TokenTree<'a>>),
+    Leaf(Token<'a>),
+}
+
+impl<'a> From<Token<'a>> for TokenTree<'a> {
+    fn from(token: Token<'a>) -> TokenTree<'a> {
+        TokenTree::Leaf(token)
+    }
+}
+
+pub fn deserialize(input: Input) -> anyhow::Result<TokenTree> {
+    match ptree(input) {
+        Ok((_, result)) => Ok(result),
+        Err(nom::Err::Error(e)) | Err(nom::Err::Failure(e)) => anyhow::bail!("{:?}", e),
+        Err(nom::Err::Incomplete(_)) => unreachable!(),
+    }
+}
+
+pub fn serialize(tree: &TokenTree, writer: &mut impl io::Write) -> io::Result<()> {
+    match tree {
+        TokenTree::Leaf(bytes) => {
+            writer.write_all(bytes.len().to_string().as_bytes())?;
+            writer.write_all(b":")?;
+            writer.write_all(bytes)?;
+        }
+        TokenTree::Node(children) => {
+            writer.write_all(b"(")?;
+            for child in children {
+                serialize(&child, writer)?;
+            }
+            writer.write_all(b")")?;
+        }
+    }
+    Ok(())
+}
+
+fn ptree(input: Input) -> IResult<Input, TokenTree> {
+    alt((pnode, pleaf))(input)
+}
+
+fn pnode(input: Input) -> IResult<Input, TokenTree> {
+    let (input, _) = tag(b"(")(input)?;
+    let mut it = iterator(input, ptree);
+    let children: Vec<_> = it.collect();
+    let (input, ()) = it.finish()?;
+    let (input, _) = tag(b")")(input)?;
+    Ok((input, TokenTree::Node(children)))
+}
+
+fn pleaf(input: Input) -> IResult<Input, TokenTree> {
+    let (input, v) = ptoken(input)?;
+    Ok((input, v.into()))
+}
+
+fn ptoken(input: Input) -> IResult<Input, Token> {
+    let (input, size) = take_while1(is_digit)(input)?;
+    let size: usize = std::str::from_utf8(size).unwrap().parse().unwrap();
+    let (input, _) = tag(b":")(input)?;
+    let (input, result) = take(size)(input)?;
+    Ok((input, result))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{TokenTree::*, *};
+
+    #[test]
+    fn parse_leaf() {
+        let input = b"12:foobarbazbizqux";
+        let v = deserialize(&input[..]).unwrap();
+        assert_eq!(v, Leaf(b"foobarbazbiz"));
+    }
+
+    #[test]
+    fn parse_simple_node() {
+        let input = b"(6:foobar)";
+        let v = deserialize(&input[..]).unwrap();
+        assert_eq!(v, Node(vec![Leaf(b"foobar")]));
+    }
+
+    #[test]
+    fn parse_node_with_leaves() {
+        let input = b"(6:foobar3:qux4:quux)";
+        let v = deserialize(&input[..]).unwrap();
+        assert_eq!(v, Node(vec![Leaf(b"foobar"), Leaf(b"qux"), Leaf(b"quux")]));
+    }
+
+    #[test]
+    fn parse_complex_node() {
+        let input = b"(6:foobar(3:qux4:quux)(2:xy))";
+        let v = deserialize(&input[..]).unwrap();
+        assert_eq!(
+            v,
+            Node(vec![
+                Leaf(b"foobar"),
+                Node(vec![Leaf(b"qux"), Leaf(b"quux"),]),
+                Node(vec![Leaf(b"xy")])
+            ])
+        );
+    }
+
+    #[test]
+    fn parse_node_start_with_subnode() {
+        let input = b"((3:foo3:bar)3:qux(4:quux))";
+        let v = deserialize(&input[..]).unwrap();
+        assert_eq!(
+            v,
+            Node(vec![
+                Node(vec![Leaf(b"foo"), Leaf(b"bar")]),
+                Leaf(b"qux"),
+                Node(vec![Leaf(b"quux"),]),
+            ])
+        );
+    }
+
+    #[test]
+    fn serialize_leaf() {
+        let mut output: Vec<u8> = Vec::new();
+        serialize(&Leaf(b"foobar"), &mut output).unwrap();
+        assert_eq!(&output, b"6:foobar");
+    }
+    #[test]
+    fn serialize_node() {
+        let mut output: Vec<u8> = Vec::new();
+        serialize(&Node(vec![]), &mut output).unwrap();
+        assert_eq!(&output, b"()");
+    }
+    #[test]
+    fn serialize_node_with_children() {
+        let mut output: Vec<u8> = Vec::new();
+        serialize(
+            &Node(vec![
+                Leaf(b"foobar"),
+                Node(vec![Leaf(b"comment"), Leaf(b"qux")]),
+            ]),
+            &mut output,
+        )
+        .unwrap();
+        assert_eq!(&output, b"(6:foobar(7:comment3:qux))");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,157 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+mod keyfile;
+use keyfile::TokenTree;
+
+const GPG_COMMENT_FIELD: &[u8] = b"comment";
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    file: PathBuf,
+    comment: Option<String>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let opt = Opt::from_args();
+    let content = std::fs::read(&opt.file)?;
+    let mut tree = keyfile::deserialize(&content)?;
+    if let Some(comment) = opt.comment {
+        upsert_comment(&mut tree, &comment);
+        let mut writer = std::fs::File::create(&opt.file)?;
+        keyfile::serialize(&tree, &mut writer)?;
+    } else {
+        println!("{}", get_comment(&tree).as_deref().unwrap_or("(none)"));
+        // if let Some(comment) = get_comment(&tree) {
+        // println!("{}", comment);
+        // }
+    }
+    Ok(())
+}
+
+fn get_comment(tt: &TokenTree) -> Option<String> {
+    use keyfile::TokenTree::*;
+    let values = if let Node(children) = tt {
+        children
+    } else {
+        return None;
+    };
+    for value in values.iter().skip(1) {
+        match value {
+            Node(xs) if xs.get(0) == Some(&Leaf(GPG_COMMENT_FIELD)) => match xs.get(1) {
+                Some(Leaf(bs)) => return Some(String::from_utf8_lossy(bs).to_string()),
+                _ => return None,
+            },
+            _ => {}
+        }
+    }
+    None
+}
+
+fn upsert_comment<'a>(tt: &mut TokenTree<'a>, value: &'a str) {
+    use keyfile::TokenTree::*;
+    let children = if let Node(children) = tt {
+        children
+    } else {
+        return;
+    };
+    // Look for an existing comment and replace it with the new one
+    for child in children.iter_mut() {
+        match child {
+            Node(xs) if xs.get(0) == Some(&Leaf(GPG_COMMENT_FIELD)) => {
+                *xs = vec![Leaf(GPG_COMMENT_FIELD), Leaf(value.as_bytes())];
+                return;
+            }
+            _ => {}
+        }
+    }
+    // Didn't find a comment, insert at the end
+    children.push(Node(vec![Leaf(GPG_COMMENT_FIELD), Leaf(value.as_bytes())]));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use keyfile::TokenTree::*;
+
+    #[test]
+    fn get_comment_exist() {
+        let tree = TokenTree::Node(vec![
+            TokenTree::Leaf(b"private-key"),
+            TokenTree::Node(vec![
+                TokenTree::Leaf(b"notcomment"),
+                TokenTree::Leaf(b"foobar"),
+            ]),
+            TokenTree::Leaf(b"somevalue"),
+            TokenTree::Node(vec![
+                TokenTree::Leaf(b"comment"),
+                TokenTree::Leaf(b"foobar"),
+            ]),
+        ]);
+        assert_eq!(get_comment(&tree), Some(String::from("foobar")));
+    }
+
+    #[test]
+    fn get_comment_missing_value() {
+        let tree = TokenTree::Node(vec![
+            TokenTree::Leaf(b"private-key"),
+            TokenTree::Node(vec![TokenTree::Leaf(b"comment")]),
+        ]);
+        assert_eq!(get_comment(&tree), None);
+    }
+
+    #[test]
+    fn get_comment_missing() {
+        let tree = TokenTree::Node(vec![
+            TokenTree::Leaf(b"private-key"),
+            TokenTree::Node(vec![
+                TokenTree::Leaf(b"notcomment"),
+                TokenTree::Leaf(b"foobar"),
+            ]),
+        ]);
+        assert_eq!(get_comment(&tree), None);
+    }
+
+    #[test]
+    fn get_comment_leaf() {
+        let tree = TokenTree::Leaf(b"qux");
+        assert_eq!(get_comment(&tree), None);
+    }
+
+    #[test]
+    fn upsert_comment_insert() {
+        let mut tree = Node(vec![
+            Leaf(b"private-key"),
+            Node(vec![Leaf(b"notcomment")]),
+            Leaf(b"otherthing"),
+        ]);
+        upsert_comment(&mut tree, "foobar");
+        assert_eq!(
+            tree,
+            Node(vec![
+                Leaf(b"private-key"),
+                Node(vec![Leaf(b"notcomment"),]),
+                Leaf(b"otherthing"),
+                Node(vec![Leaf(b"comment"), Leaf(b"foobar"),]),
+            ])
+        );
+    }
+
+    #[test]
+    fn upsert_comment_update() {
+        let mut tree = Node(vec![
+            Leaf(b"private-key"),
+            Node(vec![Leaf(b"comment"), Leaf(b"quux")]),
+            Leaf(b"otherthing"),
+        ]);
+        upsert_comment(&mut tree, "foobar");
+        assert_eq!(
+            tree,
+            Node(vec![
+                Leaf(b"private-key"),
+                Node(vec![Leaf(b"comment"), Leaf(b"foobar")]),
+                Leaf(b"otherthing"),
+            ])
+        );
+    }
 }
