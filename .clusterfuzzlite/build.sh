934da743ee0ef80fe09d46d221275c9f1a58db39
Grégoire Détrez <gregoire@fripost.org>
2023-01-25 23:00:48 +0100

    typo

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -2,4 +2,4 @@
 
 cd $SRC/gpg-keytag
 cargo fuzz build -O
-cp fuzz/target/x86_64-unknown-linux-gnu/release/{deerialization,serialization_round_trip} $OUT/
+cp fuzz/target/x86_64-unknown-linux-gnu/release/{deserialization,serialization_round_trip} $OUT/
